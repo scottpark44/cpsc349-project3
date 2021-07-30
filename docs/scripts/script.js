9cffb48e5b73f23f4845243d39638479dd7893fb
Scott Park <36457105+scottpark44@users.noreply.github.com>
2021-07-29 20:43:00 -0700

    Pushing CHanges to JS

---
+++ docs/scripts/script.js
@@ -0,0 +1,17 @@
+import * as mockroblog from './mockroblog.js'
+
+
+let timeline = mockroblog.getPublicTimeline()
+
+for(let i = 0; i < timeline.length; i++){
+    // Create an <p></p>
+    let foo = document.createElement("p");
+    // Retrieve text from an array from getPublicTimeline()
+    let text = document.createTextNode(timeline[i].text);
+    // Add the text to the <p></p>
+    foo.appendChild(text);
+    // Find an element with ID "posts", and place the <p></p> in there.
+    let element = document.getElementById("posts");
+    element.appendChild(foo);
+}
+
