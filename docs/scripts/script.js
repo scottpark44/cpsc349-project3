8f33800c85d9ce9d7bc1fcee1b62334c047a7a85
Scott Park <36457105+scottpark44@users.noreply.github.com>
2021-07-29 21:00:41 -0700

    More Changes
    Adding stuff to display user name

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -6,7 +6,7 @@ for(let i = 0; i < timeline.length; i++){
     // Create an <p></p>
     let foo = document.createElement("p");
     // Retrieve text from an array from getPublicTimeline()
-    let text = document.createTextNode(timeline[i].text);
+    let text = document.createTextNode(timeline[i].text + " - " + mockroblog.getUserName(timeline[i].user_id));
     // Add the text to the <p></p>
     foo.appendChild(text);
     // Find an element with ID "posts", and place the <p></p> in there.
