cc68b106a6a50e2f3cf65cda98cdc9e63b46ac6f
austin-officegood <scottpark44@csu.fullerton.edu>
2021-07-31 16:18:02 -0700

    Added Comments for Clarity

--- docs/scripts/user_info.js
+++ docs/scripts/user_info.js
@@ -13,43 +13,25 @@ let tailwind_class = mockroblog.tailwind_css();
     (let a = element.innerHTML), then use that variable to call information.
 */
 for(let i = 0; i < 3; i++){
+    // making div and button
     let foo = document.createElement("div");
     let btn = document.createElement("button");
 
-    let user_name = user_list[i];
-
-
-    //btn.onclick = mockroblog.display_user_data(user_list[i]);
+    //  setting btn to have the username inside
     btn.innerHTML = user_list[i];
 
-    let output_test = 'mockroblog.display_user_data("'+ user_name +'")';
-
-    console.log(output_test);
-
-
-
-
-    // Retrieve text from an array from getdivublicTimeline()
-    // let text = document.createTextNode(user_list[i]);
-    // Add the text to the <div></div>
-    //let text = document.createTextNode(user_list[i]);
-    //
-    //console.log(user_name);
-    
-    btn.innerHTML = user_name;
-
-
+    //  Adding onclick to button (display_user_data is in mockroblog.js)
+    //btn.onclick = mockroblog.display_user_data(user_list[i]);
 
     // Adding classes to foo
     for(let i = 0; i < tailwind_class.length; i++){
         foo.classList.add(tailwind_class[i]);
     }
-    //foo.appendChild(text);
-    // Find an element with ID "posts", and place the <div></div> in there.
-    
-    
+
+    //  Add button to div.
     foo.appendChild(btn);
-    
+
+    // Find an element with ID "posts", and place the <div></div> in there.
     let element = document.getElementById("posts");
     element.appendChild(foo);
 }
