21bb58858ebcbc07ad10a490807ad3321bb21cd8
Chris Nutter <cdnutter@gmail.com>
2021-07-31 19:53:20 -0700

    ay

--- docs/scripts/auth.js
+++ docs/scripts/auth.js
@@ -1,17 +1,16 @@
 import * as mockroblog from './mockroblog.js'
 
-let username = document.getElementById("username").value;
-let password = document.getElementById("password").value; // fucking plaintext
 let wrong = document.getElementById("wrong");
 let submit = document.getElementById("submit");
 
-let authenticate = authenticateUser(username, password);
-
-if (submit) {
-	submit.addEventListener("click",auth);
-}
+if (submit) { submit.addEventListener("click",auth); }
 
 function auth() {
-	if authenticate === null { wrong.innerHTML = "Wrong username or password"; }
-	else { wrong.innerHTML = "hurray"; }
+	
+	let username = document.getElementById("username").value;
+	let password = document.getElementById("password").value; // fucking plaintext
+
+	console.log("username: " + username + ", password: " + password);
+	if (mockroblog.authenticateUser(username, password) === null) { wrong.innerHTML = "<br><center>Wrong username or password</center>"; }
+	else { wrong.innerHTML = "<meta http-equiv='Refresh' content='0; url='/' />" }
 }
