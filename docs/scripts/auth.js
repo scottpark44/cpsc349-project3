abea3c0b29f0202378848df7857839ddc3ad9219
Chris Nutter <cdnutter@gmail.com>
2021-07-31 21:20:02 -0700

    brah

--- docs/scripts/auth.js
+++ docs/scripts/auth.js
@@ -1,17 +1,28 @@
 import * as mockroblog from './mockroblog.js'
 
 let wrong = document.getElementById("wrong");
-let submit = document.getElementById("submit");
+let submit_login = document.getElementById("submit_login");
+let submit_up = document.getElementById("submit_up");
 
-if (submit) { submit.addEventListener("click",auth); }
+if (submit_login) { submit_login.addEventListener("click",auth_login); }
+if (submit_up) { submit_up.addEventListener("click",auth_up); }
 
-function auth() {
+function auth_login() {
 	
 	let username = document.getElementById("username").value;
-	let password = document.getElementById("password").value; // fucking plaintext
+	let password = document.getElementById("password").value; // plaintext lmao
 
-	console.log("username: " + username + ", password: " + password);
 	if (mockroblog.authenticateUser(username, password) === null) { wrong.innerHTML = "<br><center>Wrong username or password</center>"; }
-	else { wrong.innerHTML = "<meta http-equiv='Refresh' content='0; url='/' />" }
+	else { location.replace("/"); }
+}
+
+function auth_up() {
+	
+	let username = document.getElementById("username").value;
+	let email = document.getElementById("email").value;
+	let password = document.getElementById("password").value; // plaintext lmao
+
+	mockroblog.createUser(username, email, password);
+	location.replace("/pages/login.html");
 }
 
